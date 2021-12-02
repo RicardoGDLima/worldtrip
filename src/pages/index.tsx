e38913cd17d93fe01deca55fc943ced30a08ddc8
Ricardo <rickgomeslima@gmail.com>
2021-12-01 23:18:39 -0300

    swiper slide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading  } from "@chakra-ui/react";
+import Separador from "../components/Separador";
 import Attractive from "../components/Attractive";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Slider from "../components/Slider"
 
 export default function Home() {
   return (
@@ -9,6 +11,14 @@ export default function Home() {
       <Header />
       <Banner />
       <Attractive />
+
+      <Separador />
+
+      <Heading textAlign="center" fontWeight="500" mb={["5", "14"]} fontSize={["lg", "3xl", "4xl"]} >
+        Vamos nessa? <br /> Então escolha seu continente
+      </Heading>
+
+      <Slider />
 
     </Flex>
   )
