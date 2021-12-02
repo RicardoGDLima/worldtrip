d59f438a20064051fcdaacadf0c6622b533dca23
Ricardo <rickgomeslima@gmail.com>
2021-12-02 11:44:17 -0300

    Projeto Finalizado

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -14,7 +14,7 @@ export default function Slider() {
             <Swiper slidesPerView={1} navigation pagination={{ clickable: true }} autoplay={{ delay: 5000 }} style={{ width: '100%', flex: '1' }}>
                 <SwiperSlide>
                     <Flex w="100%" h="100%" align="center" justify="center" direction="column" bgImage="url(/europe.png)" bgRepeat="no-repeat" bgSize="cover" textAlign="center" bgPosition="center">
-                        <Link href="/continent/europe">
+                        <Link href="/continent">
                             <a>
                                 <Heading fontSize={["3xl", "4xl", "5xl"]} color="gray.100" fontWeight="bold" >Europa</Heading>
                                 <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem", "1xl", "2xl"]} mt={["2", "4"]} >O continente mais antigo.</Text>
@@ -25,7 +25,7 @@ export default function Slider() {
 
                 <SwiperSlide>
                     <Flex w="100%" h="100%" align="center" justify="center" direction="column" bgImage="url(/europe.png)" bgRepeat="no-repeat" bgSize="cover" textAlign="center" bgPosition="center">
-                        <Link href="/continent/europe">
+                        <Link href="/continent">
                             <a>
                                 <Heading fontSize={["3xl", "4xl", "5xl"]} color="gray.100" fontWeight="bold" >Europa</Heading>
                                 <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem", "1xl", "2xl"]} mt={["2", "4"]} >O continente mais antigo.</Text>
