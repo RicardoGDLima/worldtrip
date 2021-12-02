e1d250dbc429719ec0cbfec2e2111ea8010a3613
Ricardo <rickgomeslima@gmail.com>
2021-12-02 10:55:04 -0300

    Header page continent

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,28 @@
-import { Flex, Grid, Image } from "@chakra-ui/react";
+import { Flex, Grid, Image, Icon } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+import { RiArrowLeftSLine } from 'react-icons/ri'
+
+import Link from 'next/link'
 
 export default function Header() {
+    const { asPath } = useRouter()
+    const notHomePage = asPath !== "/"
+
+
+
     return(
         <Flex bg="white" w="100%" as="header" mx="auto" px="1rem" h={["50px", "100px"]} align="center" justify="center" >
             <Grid h="100%" mx="auto" w="100%" maxW="1160px" alignItems="center" templateColumns="repeat(3, 1fr)" justifyContent="center" >
+
+            { notHomePage && (
+                <Link href="/">
+                <a>
+                    <Icon as={RiArrowLeftSLine} fontSize={[20, 40]} justifySelf="start" />
+                </a>
+                </Link>
+            )}
+
                 <Image w={["81px", "184px"]} src="./logo.svg" alt="Logo" justifySelf="center" gridColumn="2">
 
                 </Image>
